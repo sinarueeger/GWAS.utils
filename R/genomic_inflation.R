49b6c97827944784ae6684df670fb7fe6cfa8bd1
Sina Rüeger <sina.rueeger@gmail.com>
2018-11-28 11:16:35 +0100

    adding manual genomic inflation

---
+++ R/genomic_inflation.R
@@ -0,0 +1,34 @@
+
+
+#' Genomic inflation factor
+#'
+#' @param Z Numeric vector of arbitrary length. Z-statistics (sign does not matter)
+#' @param P Numeric vector of arbitrary length. Alternatively, the P-values can be provided, which will be transformed into Z-statistics
+#'
+#' @return value
+#' @export
+#' @details see
+#'
+#' @examples
+#'
+#' data("giant")
+#' genomic_inflation(Z = giant$BETA/giant$SE)
+#' ## should give the same as
+#' genomic_inflation(P = giant$P)
+#'
+genomic_inflation <- function(Z = NULL, P = NULL) {
+
+    if(is.null(Z) & !is.null(P)) {
+        Z <- qnorm(P/2)  ## because Z is squared after we don't care about the sign
+    }
+
+    if (is.null(Z) & is.null(P)) {
+      error("You have to pass on Z or P.")
+    }
+
+    lambda <- median(Z^2) / qchisq(0.5, 1)
+    return(lambda)
+  }
+
+
+
